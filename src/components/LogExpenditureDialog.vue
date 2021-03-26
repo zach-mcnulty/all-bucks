2e5d9df71937faa13b85c2a996377907d5a10533
Zachary McNulty <zmcnulty@indepmo.org>
2021-03-26 10:25:11 -0500

    LogExpenditureDialog components

---
+++ src/components/LogExpenditureDialog.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+  <v-dialog v-if="screenSize === 'xs'" v-model="dialog" fullscreen transition="dialog-bottom-transition">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        :class="[screenSize === 'xs' ? 'd-block' : iconToggle, 'ml-2']"
+        height="24"
+        elevation="0"
+        fab
+        x-small
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </template>
+    <log-expenditure-dialog-form :screenSize="screenSize"></log-expenditure-dialog-form>
+  </v-dialog>
+  <v-dialog v-else v-model="dialog" width="500" transition="dialog-bottom-transition">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        :class="[screenSize === 'xs' ? 'd-block' : iconToggle, 'ml-2']"
+        height="24"
+        elevation="0"
+        fab
+        x-small
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </template>
+    <log-expenditure-dialog-form :screenSize="screenSize"></log-expenditure-dialog-form>
+  </v-dialog>
+  </div>
+</template>
+
+<script>
+import LogExpenditureDialogForm from "./LogExpenditureDialogForm.vue";
+
+export default {
+  props: ['iconToggle'],
+
+  components: {
+    LogExpenditureDialogForm
+  },
+
+  data() {
+    return {
+      dialog: false
+    }
+  },
+
+  computed: {
+    screenSize() {
+      return this.$vuetify.breakpoint.name;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/LogExpenditureDialogForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card>
+    <v-card-title class="headline green">
+      Log an Expenditure
+      <v-card-actions v-if="screenSize === 'xs'">
+
+        <v-btn color="primary" text @click="dialog = false"> I accept </v-btn>
+      </v-card-actions>
+    </v-card-title>
+
+    <form>
+      <v-text-field placeholder="merchant"></v-text-field>
+      <v-text-field placeholder="amount"></v-text-field>
+      <v-textarea placeholder="notes" rows="1" auto-grow></v-textarea>
+    </form>
+
+    <v-divider></v-divider>
+
+    <v-card-actions v-if="screenSize !== 'xs'">
+      <v-spacer></v-spacer>
+      <v-btn color="primary" text @click="dialog = false"> I accept </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['screenSize'],
+
+  data() {
+    return {
+      merchant: "",
+      amount: "",
+      notes: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
